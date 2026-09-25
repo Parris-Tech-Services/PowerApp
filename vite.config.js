209c615309cf7a716202c49bfcbb62ade178be2b
Josh <josh@example.com>
2026-09-25 16:33:06 +1000

    Add vite plugin legacy for older browser support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 /* global process */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    })
+  ],
   base: process.env.GITHUB_ACTIONS ? '/PowerApp/' : '/',
 })
